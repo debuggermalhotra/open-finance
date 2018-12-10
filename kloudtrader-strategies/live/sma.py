242de9952aa523d98f7113290248398756e37ee2
Chetan Malhotra <chetanalwaysmalhotra@hotmail.com>
2018-12-11 00:30:53 +0530

    commit

--- kloudtrader-strategies/live/sma.py
+++ kloudtrader-strategies/live/sma.py
@@ -3,12 +3,14 @@ from collections import deque
 import pandas as pd
 from kloudtrader.defaults import ACCESS_TOKEN
 import datetime
+import itertool
 
 df=pd.DataFrame()
-short_window=9
-long_window=21
-askPriceBuffer_size=long_window+50
+short_window=2
+long_window=4
+askPriceBuffer_size=long_window+10
 askPriceBuffer=deque(maxlen=askPriceBuffer_size)
+SignalBuffer=deque(maxlen=25)
 
 
 def live_feed_handler(symbol):    
@@ -29,12 +31,29 @@ for quotes in live_feed_handler('AAPL'):
     df['Sell']=df.apply(lambda y : 1 if y['SMA']<y['LMA'] and y['SMA2']>y['LMA2'] else 0,axis=1)
     df['Signal']=df['Buy']+df['Sell']
     for index,row in df.iterrows():
-        if row['SMA']>row['LMA'] and row['SMA2']<row['LMA2']:
-            if row['SMA']>row['ask']:
-                print('LONG signal at {}'.format(datetime.datetime.now()))
-        if row['SMA']<row['LMA'] and row['SMA2']>row['LMA2']:
-            if row['SMA']<row['ask']:
-                print("SHORT signal at {}".format(datetime.datetime.now())) 
+        if row['SMA']>row['LMA'] and row['SMA2']<row['LMA2'] and row['SMA']>row['ask']:
+            SignalBuffer.appendleft("LONG")
+            
+           
+        elif row['SMA']<row['LMA'] and row['SMA2']>row['LMA2']and row['SMA']<row['ask']:
+            SignalBuffer.appendleft("SHORT")
+          
+
+        if len(SignalBuffer)==SignalBuffer.maxlen:
+            '''
+            short_count=0
+            long_count=0
+            for ele in list(SignalBuffer): 
+                if ele == "SHORT": 
+                    short_count = short_count + 1
+                elif ele == "LONG":
+                    long_count = long_count + 1
+            print("Long count {}".format(long_count)
+            print("Short count {}".format(short_count))
+            '''
+            max(len(list(v)) for g,v in itertools.groupby(list(SignalBuffer)))
+            SignalBuffer.pop()
+             
 
                 
             
